5589683c327c7f19967d95a872f73465298572a6
nianchenhao <robert870724@gmail.com>
2023-01-03 21:50:25 +0800

    Initial Commit

---
+++ RickAndMorty/Models/RMCharacter.swift
@@ -0,0 +1,8 @@
+//
+//  RMCharacter.swift
+//  RickAndMorty
+//
+//  Created by 粘辰晧 on 2023/1/3.
+//
+
+import Foundation
---
+++ RickAndMorty/Models/RMEpisode.swift
@@ -0,0 +1,8 @@
+//
+//  RMEpisode.swift
+//  RickAndMorty
+//
+//  Created by 粘辰晧 on 2023/1/3.
+//
+
+import Foundation
